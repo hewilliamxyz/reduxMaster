48f6a62d95995e59659f451183510170415f2544
William <he.william@yahoo.com>
2017-11-03 15:41:33 -0700

    added comment of what state looks like

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -2,6 +2,7 @@ import { combineReducers } from 'redux';
 import BooksReducer from './booksReducer';
 import ActiveBookReducer from './activeBookReducer'
 
+//what the state looks like
 const rootReducer = combineReducers({
   books: BooksReducer,
   activeBook: ActiveBookReducer
