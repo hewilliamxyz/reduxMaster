f7c671761868ba3cfe5421bc0d3cbb8c33c6bfe6
William <he.william@yahoo.com>
2017-11-03 15:38:24 -0700

    added bookDetail

--- src/components/app.js
+++ src/components/app.js
@@ -1,11 +1,13 @@
 import React, { Component } from 'react';
-import BooksList from '../containers/booksList'
+import BooksList from '../containers/booksList';
+import BookDetail from '../containers/bookDetail';
 
 export default class App extends Component {
   render() {
     return (
       <div>
         <BooksList/>
+        <BookDetail/>
       </div>
     );
   }
